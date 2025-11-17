fdaa6dd21c08e620315ff0a11dc66124a69999dd
Abdel Djalil Chafai <djalilchafai9@gmail.com>
2025-11-17 00:58:29 +0000

    Add files via upload

---
+++ Functions_calc.py
@@ -0,0 +1,49 @@
+
+def add(*args): 
+    """Adds two numbers"""
+    return sum(args)
+def subtract(*args):
+    """Subtracts two numbers"""
+    print("Reached subtract function")
+    #result = first arg 
+    #for each argument after the first arg 
+        #subtract that number from the result and store it as the new result 
+    #return result
+    result = arg[0]
+    for num in arg[1:]:
+        result = result - num
+
+    return result 
+
+def mult(*args): 
+    result = math.prod(args)
+
+    return result 
+
+def div(*args): 
+    result = args[0]
+    for num in arg[1:]: 
+        if num != 0:
+            result = result/num            
+        else: print("Error")
+
+#interface
+operation = input("which operation would you like to input          insert operation:")
+numbers = input("What numbers would you like to execute the operation on?  insert number or list of numbers:")
+
+#For numbers, it is being stored as a string but it needs to be passed as the argument for the functions which require list "*args" meaning i have to create another variable to split the string into a list
+split_numbers = numbers.split() 
+
+op = operation.strip().lower()
+if op == div: 
+    print(div(split_numbers))
+elif op == mult: 
+    print(mult(split_numbers)) 
+elif op == add: 
+    print(add(split_numbers))
+elif op == subtract: 
+    print(subtract(split_numbers))
+
+#debugging: 
+print(numbers) 
+print(type(numbers))
